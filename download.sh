9197d115e7ed8b216c499d0bad508624e00b1ffe
daxit <daxitm2112@gmail.com>
2023-07-24 21:45:00 +0530

    added extension

--- download.sh
+++ download.sh
@@ -12,3 +12,6 @@ set -Eeuo pipefail
 echo "Downloading, this might take a while..."
 
 aria2c -x 10 --disable-ipv6 --input-file ./links.txt --dir ./models --continue
+
+git clone https://github.com/civitai/sd_civitai_extension.git ./extensions/sd_civitai_extension
+git clone https://github.com/imrayya/stable-diffusion-webui-Prompt_Generator.git ./extensions/Prompt_Generator
