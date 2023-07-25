10c48edd525f464663e946c756076d4849a4505e
daxit <daxitm2112@gmail.com>
2023-07-25 14:42:10 +0530

    fix bugs in extension file

--- download.sh
+++ download.sh
@@ -18,7 +18,7 @@ git clone https://github.com/imrayya/stable-diffusion-webui-Prompt_Generator.git
 git clone https://github.com/Mikubill/sd-webui-controlnet.git ./extensions/controlnet
 git clone https://github.com/Uminosachi/sd-webui-inpaint-anything.git ./extensions/inpaint-anything
 git clone https://github.com/richrobber2/canvas-zoom.git ./extensions/canvas-zoom
-git clone https://github.com/deforum-art/sd-webui-deforum-art.git ./extensions/deforum
+git clone https://github.com/deforum-art/sd-webui-deforum.git ./extensions/deforum
 git clone https://github.com/s0md3v/sd-webui-roop.git ./extensions/roop
 git clone https://github.com/dustysys/ddetailer.git ./extensions/ddetailer
 git clone https://github.com/fkunn1326/openpose-editor.git ./extensions/openpose-editor
